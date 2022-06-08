1d70be5d2da5a5db4ec9eb30a89e9af27be6fada
gilsonk <73434967+gilsonk@users.noreply.github.com>
2022-06-08 19:15:57 +0200

    Update Sphinx config and README

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Install dependencies:
 
 Then run **cv_builder.py**:
 
-    python cv_builder.py
+    python cv_builder/cv_builder.py
 
 ### (Optional) Compiling it yourself
 Install PyInstaller:
@@ -40,7 +40,7 @@ Install PyInstaller:
 
 From within the cv_builder folder, run:
 
-    python -m pyinstaller --onefile --windowed cv_builder.py
+    python -m pyinstaller --onefile --windowed cv_builder/cv_builder.py
 
 ## Data Model
 In order to store CVs, the project define several custome classes, stored under a JSON format:
--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../cv_builder"))
 
 
 # -- Project information -----------------------------------------------------
@@ -24,7 +24,7 @@ copyright = "2022, Gilson, Kevin"
 author = "Gilson, Kevin"
 
 # The full version, including alpha/beta/rc tags
-release = "0.5.0"
+release = "0.5.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -52,7 +52,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
